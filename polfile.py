545882ee1211d90a782b099e5b85704d2d0eee45
kunalkumarkundan31 <kunalkumarkundan@gmail.com>
2022-03-25 21:57:36 +0530

    Update polfile.py

--- polfile.py
+++ polfile.py
@@ -23,6 +23,5 @@ if __name__ == '__main__':
     main(username=args.u,password=args.p,organizationId=args.o,environmentId=args.e,assetId=args.at,policyFilePath=args.pp)
 
     
- def main(username,password,policyFilePath,organizationId,environmentId,assetId):
-    print("aaa")
-    return 0
+def main(username,password,policyFilePath,organizationId,environmentId,assetId):
+    print("kkk")
