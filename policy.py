30974a2daa15742cdf05feefbc69353e95e06f6c
kunalkumarkundan31 <kunalkumarkundan@gmail.com>
2022-03-28 16:15:25 +0530

    Update policy.py

--- policy.py
+++ policy.py
@@ -130,4 +130,5 @@ if __name__ == '__main__':
     parser.add_argument('--pp', metavar='policyFilePath', required=True,
                         help='policy file location')
     args = parser.parse_args()
-    main(username=args.u,password=args.p,organizationId=args.o,environmentId=args.e,assetId=args.at,policyFilePath=args.pp)
+    return main(username=args.u,password=args.p,organizationId=args.o,environmentId=args.e,assetId=args.at,policyFilePath=args.pp)
+    
